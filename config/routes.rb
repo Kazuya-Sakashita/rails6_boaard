c173d8579373dedbae04616d548fcf4c0f2dde60
Kazuya-Sakashita <kz0508@gmail.com>
2021-05-13 07:14:23 +0900

    add communities and comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'communities#index'
+  resources :communities, only: %i[index new create show] do
+    resources :comments, only: %i[new create]
+  end
 end
